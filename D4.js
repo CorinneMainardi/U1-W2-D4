68c403ffac2590e58032af18ccda99839fa966b4
Corinne Mainardi <mainardi.corinne@gmail.com>
2024-08-22 15:32:27 +0200

    esercizi finiti

--- D4.js
+++ D4.js
@@ -50,6 +50,15 @@ console.log(crazyDiff(5));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function boundary(n) {
+  if ((n > 20 && n < 100) || n === 400) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(boundary());
+
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -58,18 +67,45 @@ console.log(crazyDiff(5));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function epify(string = "ciao") {
+  if (string != "epicode") {
+    const string2 = string.concat(" epicode");
+    return string2;
+  } else {
+    return string;
+  }
+}
+console.log(epify());
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(num) {
+  num > 0;
+  if (num % 3 === 0) {
+    return num + " è multiplo di di 3";
+  } else if (num % 7 === 0) {
+    return num + " è multiplo  di 7";
+  } else {
+    return num + " non è multiplo di 3 o di 7";
+  }
+}
+console.log(check3and7(20));
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function reverseString(string = "supercalifragilistichespiralidoso") {
+  const revS = string.split("").reverse().join("");
+  return revS;
+}
+console.log(reverseString());
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -78,6 +114,14 @@ console.log(crazyDiff(5));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function upperFirst(string = "il cane dorme sul letto") {
+  return string
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+console.log(upperFirst());
+
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
@@ -85,8 +129,23 @@ console.log(crazyDiff(5));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function cutString(string = "vorrei andare al mare") {
+  string2 = string.slice(1, -1);
+  return string2;
+}
+console.log(cutString());
+
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function giveMeRandom(lenght) {
+  let arrayN = [];
+  lenght = 15;
+  for (let i = 0; i < lenght; i++) {
+    arrayN.push(Math.floor(Math.random() * 11));
+  }
+  return arrayN;
+}
+console.log(giveMeRandom());
